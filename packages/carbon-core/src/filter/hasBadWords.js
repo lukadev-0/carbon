6b20b265f5421435b02582ac03efb9bbe396b71f
MarsRon <71264052+MarsRon@users.noreply.github.com>
2021-03-29 21:43:09 +0800

    Shorten hasBadWords.js

--- packages/carbon-core/src/filter/hasBadWords.js
+++ packages/carbon-core/src/filter/hasBadWords.js
@@ -1,13 +1,10 @@
-let regExArray = []
-require('node-fetch')(
-	'https://raw.githubusercontent.com/mogade/badwords/master/en.txt'
-)
-	.then((res) => res.text())
-	.then((data) => (regExArray = data.split('\n')))
+let regExArray = [];
+require("node-fetch")("https://raw.githubusercontent.com/mogade/badwords/master/en.txt")
+	.then(res => res.text())
+	.then(data => regExArray = data.split("\n"))
 
 module.exports = function hasBadWords(content) {
-	for (const str of regExArray) {
-		if (content.match(new RegExp(str, 'gi'))) return true
-	}
-	return false
+	return regExArray.some(str =>
+		content.match(new RegExp(str, "gi"))
+	);
 }
