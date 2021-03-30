9319025b199a9a4a5891b41725ea1d7a0e12134d
throwaway thing <this.is.a.cool.mail.cool.2800325@gmail.com>
2021-03-30 16:22:59 +0000

    say if channel incorrect

--- src/commands/close.js
+++ src/commands/close.js
@@ -5,10 +5,11 @@ const { post, update } = require('../interactionHandler')
 module.exports = async (int) => {
 	await post(int, 'Closing your channel...')
 	const channel = await client.channels.fetch(int.channel_id)
-	console.log(getChannelOwner(channel), int.member.user.id)
-	console.log(getChannelOwner(channel) === int.member.user.id)
 	if (getChannelOwner(channel) === int.member.user.id) {
 		closeChannel(channel)
 		await update(int, 'Closed your channel.')
 	}
+  else {
+    await update(int, "This isn't your channel or it's not even a help channel!")
+  }
 }
