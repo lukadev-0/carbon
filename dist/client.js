d0d561ab3d6605affe9d451fff723d821038c1f6
dai <72147841+daimond113@users.noreply.github.com>
2021-05-30 09:54:07 +0200

    Delete dist directory

--- dist/client.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.client = void 0;
-const discord_js_1 = require("discord.js");
-exports.client = new discord_js_1.Client({
-    intents: new discord_js_1.Intents(discord_js_1.Intents.ALL & ~(1 << 8)),
-});
