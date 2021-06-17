915b1db9f88be0a868ee26c4c900e6d62588d00a
daimond113 <72147841+daimond113@users.noreply.github.com>
2021-06-17 13:48:55 +0200

    docs: fix SSG

--- packages/website/pages/tab/2/[type].tsx
+++ packages/website/pages/tab/2/[type].tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Typography, useTheme } from '@material-ui/core'
-import { useRouter } from 'next/router'
 import DeveloperHeader from '../../../src/DeveloperHeader'
 import CodeBlock from '../../../src/CodeBlock'
 import FindDoc from '../../../src/FindDoc'
@@ -9,33 +8,25 @@ import DeveloperDocElement from '../../../src/DeveloperDocElement'
 import GetTags from '../../../src/GetTags'
 import { Alert } from '@material-ui/lab'
 import Head from '../../../src/Head'
-import { allowedTypes } from '../../../src/DocsTypes'
+import { allowedTypes, Child, Child2 } from '../../../src/DocsTypes'
+import { GetStaticProps } from 'next'
 
-export default function DynamicDoc(): JSX.Element {
+interface Props {
+    type: string
+    doc: Child
+    isClass: boolean
+    codeString: string
+    comment: string | null
+    properties: Child2[]
+    methods: Child2[]
+}
+
+export default function DynamicDoc({ type: name, doc, isClass, codeString, comment, properties, methods }: Props): JSX.Element {
     const theme = useTheme()
-    const router = useRouter()
-    const { type: name } = router.query
-    const possibleDoc = FindDoc().flat()
-    const doc = possibleDoc?.find((v) => v.name === name)
-    const isClass = doc.kindString.toLowerCase() === 'class'
-    // START: code string constructing
-    const constructorKeyword = isClass ? 'new ' : ''
-    const packageName = doc.sources[0].fileName.split('/')[1]
-    const params = isClass ? doc.children.find((v) => v.kindString.toLowerCase() === 'constructor').signatures[0].parameters.map((v) => `${v.name}: ${v.type.name}`).join(', ') : ''
-    const constructor = `.${doc.name}${isClass ? `(${params})` : ''}`
-    const codeString = `${constructorKeyword}${packageName}${constructor}`
-    // END
-    const comment = doc.comment.shortText ?? doc.comment.text ?? null
-    const properties = doc.children.filter((v) =>
-        v.kindString.toLowerCase() === (isClass ? 'property' : 'enumeration member'),
-    )
-    const methods = doc.children.filter((v) =>
-        v.kindString.toLowerCase() === 'method',
-    )
     const docTags = GetTags(doc)
     return (
         <div>
-            <Head title={Array.isArray(name) ? name[0] : name} name='API docs for Carbon' description={`Carbon API docs for ${name}`} />
+            <Head title={name} name='API docs for Carbon' description={`Carbon API docs for ${name}`} />
             <DeveloperHeader>
                 <Typography variant="h5">
                     <span style={{ color: theme.palette.primary.main }}>{doc.kindString.toLowerCase()}</span> {name}
@@ -68,9 +59,39 @@ export default function DynamicDoc(): JSX.Element {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const getStaticProps: GetStaticProps<Props> = async ({ params: { type: id } }) => {
+    const docRaw = FindDoc().flat().filter((v) => allowedTypes.includes(v.kindString.toLowerCase())).filter((v) => v.name === id)
+    const doc = docRaw?.[0]
+    const isClass = doc.kindString.toLowerCase() === 'class'
+    const constructorKeyword = isClass ? 'new ' : ''
+    const packageName = doc.sources[0].fileName.split('/')[1]
+    const params = isClass ? doc.children.find((v) => v.kindString.toLowerCase() === 'constructor').signatures[0].parameters.map((v) => `${v.name}: ${v.type.name}`).join(', ') : ''
+    const constructor = `.${doc.name}${isClass ? `(${params})` : ''}`
+    const codeString = `${constructorKeyword}${packageName}${constructor}`
+    const comment = doc.comment.shortText ?? doc.comment.text ?? null
+    const properties = doc.children.filter((v) =>
+        v.kindString.toLowerCase() === (isClass ? 'property' : 'enumeration member'),
+    )
+    const methods = doc.children.filter((v) =>
+        v.kindString.toLowerCase() === 'method',
+    )
+    return {
+        props: {
+            type: Array.isArray(id) ? id[0] : id,
+            doc,
+            isClass,
+            codeString,
+            comment,
+            properties,
+            methods,
+        },
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getStaticPaths: any = async () => {
     const paths = FindDoc().flat().filter((v) => allowedTypes.includes(v.kindString.toLowerCase())).map((v) => {
-        return { id: v.name }
+        return { params: { type: v.name } }
     })
     return {
         paths,
