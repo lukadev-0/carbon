80832075b664094c7b424f9caa7d1f363820260d
daimond113 <72147841+daimond113@users.noreply.github.com>
2021-06-21 14:43:56 +0200

    fix: tests

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,12 +1,10 @@
 export { Client } from './client'
 export { CarbonError, CarbonErrorType } from './util/CarbonError'
-import { interactionHandler, defaults } from '../../interactions/dist'
+import { interactionHandler, defaults } from '@carbon-js/interactions'
 import { client } from './client'
 
 client.on('interaction', interactionHandler)
 
 client.registerCommands({ commands: defaults() })
-
-client.removeCommands()
 
 client.login('')
